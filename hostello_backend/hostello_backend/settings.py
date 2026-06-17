b9b34567ad5f9a90c3d08ce9bfb1e15539ddf54c
HoangNam2464 <hoangnam01012005@gmail.com>
2026-06-17 23:01:59 +0700

    Revert "Add temporary auto auth bypass for local testing"
    This reverts commit d3595658863919cd0adda65da359ae6934222317.

--- hostello_backend/hostello_backend/settings.py
+++ hostello_backend/hostello_backend/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'portal.middleware.DevAutoAuthBypassMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
